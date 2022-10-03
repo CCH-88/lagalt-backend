a8582c7c3c1d47e057d72bafc5c61a0b0011717b
Jack <cph-jj484@cphbusiness.dk>
2022-10-03 13:38:47 +0200

    Initial commit.

---
+++ src/test/java/jact/lagaltproject/LagaltProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jact.lagaltproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
