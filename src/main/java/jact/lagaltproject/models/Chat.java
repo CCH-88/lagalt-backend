79a9fb013a7853e0dd434269ac2eed0af00d82e4
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-10-05 11:30:18 +0100

    chat cleanup

--- src/main/java/jact/lagaltproject/models/Chat.java
+++ src/main/java/jact/lagaltproject/models/Chat.java
@@ -2,7 +2,6 @@ package jact.lagaltproject.models;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -15,9 +14,6 @@ public class Chat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Type(type = "list-array")
-    @Column(name = "chat_message", columnDefinition = "text[]")
-    private String[] chatMessages;
 
     @OneToMany(mappedBy = "chat")
     private Set<Message> messages;
