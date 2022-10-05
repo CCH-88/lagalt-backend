3a5b0b268c9277c997152d81433fc6a9768a7f75
Jack-Hagedorn <jack@hagedorn.me>
2022-10-05 14:06:19 +0200

    Restarted on mappings, after refactor in development.

--- src/main/java/jact/lagaltproject/mappers/FreelancerMapper.java
+++ src/main/java/jact/lagaltproject/mappers/FreelancerMapper.java
@@ -16,29 +16,7 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public abstract class FreelancerMapper {
 
-//    @Autowired
-//    protected FreelancerService freelancerService;
-//
-//    @Mapping(target = "projects", source = "project.id")
-//    public abstract FreelancerDTO freelancerDTO(Freelancer user);
-//
-//    public abstract Collection<FreelancerDTO> userToUserDTO(Collection<Freelancer> users);
-//
-//    @Mapping(target = "project", source = "project", qualifiedByName = "projectIdToProject")
-//    public abstract Freelancer freelancerDtoToFreelancer(FreelancerDTO dto);
-//
-//    @Named("usersToIds")
-//    Set<Long> mapFreelancersToIds(Set<Freelancer> source) {
-//        if (source == null) return null;
-//        return source.stream()
-//                .map(Freelancer::getId).collect(Collectors.toSet());
-//    }
-//
-//    @Named("userIdsToUsers")
-//    Set<Freelancer> mapIdsToFreelancers(Set<Integer> id) {
-//        return id.stream()
-//                .map(i -> freelancerService.findById(i))
-//                .collect(Collectors.toSet());
-//    }
+    @Autowired
+    protected FreelancerService freelancerService;
 
 }
--- src/main/java/jact/lagaltproject/mappers/ProjectMapper.java
+++ src/main/java/jact/lagaltproject/mappers/ProjectMapper.java
@@ -16,32 +16,9 @@ import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public abstract class ProjectMapper {
-//
-//    protected FreelancerService freelancerService;
-//    protected ProjectService projectService;
-//
-//    @Mapping(target = "project", source = "project.id")
-//    @Mapping(target = "freelancer", source = "freelancer.id")
-//    public abstract ProjectDTO projectToProjectDto(Project project);
-//
-//    public abstract Collection<ProjectDTO> projectToProjectDto(Collection<Project> projects);
-//
-//    @Mapping(target = "project", source = "project", qualifiedByName = "projectIdToProject")
-//    @Mapping(target = "freelancers", source = "freelancers", qualifiedByName = "freelancerIdsToFreelancers")
-//    public abstract Project projectDtoToProject(ProjectDTO dto);
-//
-//    //Custom Mappings
-//    @Named("projectIdToProject")
-//    Project mapIdToProject(int id) {
-//        return projectService.findById(id);
-//    }
-//
-//    @Named("freelancerIdsToFreelancers")
-//    Set<Freelancer> mapIdsToFreelancers(Set<Integer> id) {
-//        return id.stream()
-//                .map(i -> freelancerService.findById(i))
-//                .collect(Collectors.toSet());
-//    }
 
+
+    protected FreelancerService freelancerService;
+    protected ProjectService projectService;
 
 }
