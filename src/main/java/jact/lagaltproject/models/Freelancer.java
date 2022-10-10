bf77d95d2094f8692d363f9a702e98c5500c1a79
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-10-10 11:35:04 +0200

    fix: freelancer_history creation

--- src/main/java/jact/lagaltproject/models/Freelancer.java
+++ src/main/java/jact/lagaltproject/models/Freelancer.java
@@ -43,13 +43,13 @@ public class Freelancer {
     private String description;
 
     //Relationships
-    @OneToMany(mappedBy = "freelancer")
-    Set<Message> messages;
+    //@OneToMany(mappedBy = "freelancer")
+    //Set<Message> messages;
 
-    @OneToMany(mappedBy = "freelancer")
-    private Set<Project_freelancer> project_freelancers;
+    //@OneToMany(mappedBy = "freelancer")
+    //private Set<Project_freelancer> project_freelancers;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "freelancer_history_id", referencedColumnName = "id")
     private Freelancer_history freelancer_history;
 
