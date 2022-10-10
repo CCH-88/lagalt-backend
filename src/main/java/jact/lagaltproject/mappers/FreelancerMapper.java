c1f99d1ec61c73e7033e618898a41bbf270efa11
Anders Bille Lind <anders.bille.lind@gmail.com>
2022-10-10 20:52:56 +0200

    feat: start of DTOToFreelancer

--- src/main/java/jact/lagaltproject/mappers/FreelancerMapper.java
+++ src/main/java/jact/lagaltproject/mappers/FreelancerMapper.java
@@ -2,16 +2,18 @@ package jact.lagaltproject.mappers;
 
 
 import jact.lagaltproject.models.Freelancer;
+import jact.lagaltproject.models.Freelancer_history;
 import jact.lagaltproject.models.Message;
 import jact.lagaltproject.models.Project_freelancer;
 import jact.lagaltproject.models.dtos.freelancer.FreelancerDTO;
+import jact.lagaltproject.services.FreelancerHistoryService.FreelancerHistoryService;
 import jact.lagaltproject.services.freelancer.FreelancerService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Collection;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,8 +22,15 @@ public abstract class FreelancerMapper {
 
     @Autowired
     protected FreelancerService freelancerService;
+
+    @Autowired
+    protected FreelancerHistoryService freelancerHistoryService;
+
+    /*
+    *   Mapping of Freelancer To FreelancerDTO
+    */
     @Mapping(target = "messages",source = "messages", qualifiedByName = "messagesToIds")
-    @Mapping(target = "project_history", source = "freelancer_history.id")
+    @Mapping(target = "freelancer_history", source = "freelancer_history.id")
     @Mapping(target = "projectFreelancers", source = "project_freelancers", qualifiedByName = "projectFreelancersToIds")
     public abstract FreelancerDTO freelancerToDTO(Freelancer freelancer);
 
@@ -39,5 +48,17 @@ public abstract class FreelancerMapper {
             return null;
         }
         return project_freelancers.stream().map(project_freelancer -> project_freelancer.getId().getFreelancer_id() + project_freelancer.getId().getProject_id()).collect(Collectors.toSet());
+    }
+
+    /*
+    *   Mapping of Freelancer To FreelancerDTO
+    */
+
+    @Mapping(target = "freelancer_history", source = "freelancer_history", qualifiedByName = "freelancerHistoryIdToFreelancerHistory")
+    public abstract Freelancer FreelancerDTOtoFreelancer(FreelancerDTO dto);
+
+    @Named("freelancerHistoryIdToFreelancerHistory")
+    Freelancer_history mapIdToFreelancer_History(Long id ) {
+        return freelancerHistoryService.findById(id);
     }
 }
