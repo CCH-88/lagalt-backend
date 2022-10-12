2192e068ab05b188c3cacf33cf768e55a4e728bc
Jack-Hagedorn <jack@hagedorn.me>
2022-10-12 09:26:10 +0200

    Last check on branch before merge.

--- src/main/java/jact/lagaltproject/repositories/FreelancerHistoryRepository.java
+++ src/main/java/jact/lagaltproject/repositories/FreelancerHistoryRepository.java
@@ -4,9 +4,11 @@ import jact.lagaltproject.models.Freelancer;
 import jact.lagaltproject.models.Freelancer_history;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
+@Repository
 public interface FreelancerHistoryRepository extends JpaRepository<Freelancer_history, Long> {
 
 
